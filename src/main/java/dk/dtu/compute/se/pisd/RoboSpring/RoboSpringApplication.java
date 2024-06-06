65ced4b3595d8d880ec3fdf04ebba50d002e41a3
Mustafa Baker <s235092@dtu.dk>
2024-06-06 12:06:27 +0200

    Added Spring

---
+++ src/main/java/dk/dtu/compute/se/pisd/RoboSpring/RoboSpringApplication.java
@@ -0,0 +1,13 @@
+package dk.dtu.compute.se.pisd.RoboSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/dtu/compute/se/pisd/RoboSpring/RoboSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.dtu.compute.se.pisd.RoboSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
