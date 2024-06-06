19e9a9d60b48efead0016ecbe63eed3f37af6e7b
Elias Larsen <s235125@dtu.dk>
2024-06-06 12:23:56 +0200

    Changed name of main app

--- src/main/java/dk/dtu/compute/se/pisd/RoboSpring/RoboServerApp.java
+++ src/main/java/dk/dtu/compute/se/pisd/RoboSpring/RoboServerApp.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoboSpringApplication {
+public class RoboServerApp
+{
 
 	public static void main(String[] args) {
-		SpringApplication.run(RoboSpringApplication.class, args);
+		SpringApplication.run(RoboServerApp.class, args);
 	}
 
 }
