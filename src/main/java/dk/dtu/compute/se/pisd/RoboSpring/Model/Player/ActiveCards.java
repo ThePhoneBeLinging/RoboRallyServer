62fdae3e015ac46ed8a834fb7781fbe2d410f576
Elias Larsen <s235125@dtu.dk>
2024-06-09 14:01:23 +0200

    UpdatedTableNames

--- src/main/java/dk/dtu/compute/se/pisd/RoboSpring/Model/Player/ActiveCards.java
+++ src/main/java/dk/dtu/compute/se/pisd/RoboSpring/Model/Player/ActiveCards.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "cardsOnHand")
+@Table(name = "activeCards")
 @Getter
 @Setter
 @NoArgsConstructor
--- src/main/java/dk/dtu/compute/se/pisd/RoboSpring/Model/Player/discardedCards.java
+++ src/main/java/dk/dtu/compute/se/pisd/RoboSpring/Model/Player/discardedCards.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "cardsOnHand")
+@Table(name = "discardedCards")
 @Getter
 @Setter
 @NoArgsConstructor
