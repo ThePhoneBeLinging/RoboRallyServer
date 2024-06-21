9f655667385be013e8d8ed62d0a335dead687655
adel051700 <s235076@dtu.dk>
2024-06-21 10:05:28 +0200

    fixed issue where upgradeshop adds duplicates of the upgrade cards.

--- src/main/java/dk/dtu/compute/se/pisd/RoboSpring/Util.java
+++ src/main/java/dk/dtu/compute/se/pisd/RoboSpring/Util.java
@@ -25,6 +25,7 @@ public class Util
         gameBoard.setPhase(Phase.valueOf(serverBoard.getBoard().getPhase()));
         gameBoard.setGameID(serverBoard.getGameID());
         gameBoard.setTurnID(serverBoard.getTurnID());
+        serverBoard.setUpgradeCards(new ArrayList<>());
 
         for (Player player : serverBoard.getPlayerList())
         {
